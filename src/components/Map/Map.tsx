d4a7d70f0b3b1351fc1cd3a200aab152e9ffd1be
mokyoungg <mokyoungg@gmail.com>
2024-08-20 16:18:28 +0900

    feat: observer map center and get data based center location

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,34 +1,71 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getLocationBasedList } from "../../api/camping";
 
 const Map = () => {
-  const mapElement = useRef(null);
+  const { naver } = window;
 
+  const mapElement = useRef<HTMLDivElement>(null);
+  const centerRef = useRef<{ lat: number; lng: number }>({
+    lat: 37.5665,
+    lng: 126.978,
+  });
+
+  const [mapCenter, setMapCenter] = useState<{ lat: number; lng: number }>({
+    lat: 37.5665,
+    lng: 126.978,
+  });
+
+  // observer map center
   useEffect(() => {
-    const { naver } = window;
-
     if (!mapElement.current || !naver) return;
 
     const mapOptions = {
-      center: new naver.maps.LatLng(37.5665, 126.978),
+      center: new naver.maps.LatLng(
+        centerRef.current.lat,
+        centerRef.current.lng
+      ),
       zoom: 13,
     };
 
     const map = new naver.maps.Map(mapElement.current, mapOptions);
 
-    // 마커 추가 예시
-    const marker = new naver.maps.Marker({
-      position: new naver.maps.LatLng(37.5665, 126.978),
-      map: map,
+    naver.maps.Event.addListener(map, "center_changed", () => {
+      const center = map.getCenter();
+      centerRef.current = {
+        lat: center.x,
+        lng: center.y,
+      };
     });
 
     return () => {
-      // Cleanup: 마커를 삭제하거나 이벤트 리스너를 제거할 수 있음
-      marker.setMap(null);
+      // naver.maps.Event.removeListener();
     };
   }, []);
 
+  const getCenter = () => {
+    const { lat, lng } = centerRef.current;
+
+    setMapCenter({ lat: lat, lng: lng });
+  };
+
+  const { data: locationList } = useQuery({
+    queryKey: ["location-list", mapCenter],
+    queryFn: () =>
+      getLocationBasedList({
+        pageNo: 1,
+        mapX: mapCenter.lat.toString(),
+        mapY: mapCenter.lng.toString(),
+        radius: "10000",
+      }),
+  });
+
   return (
-    <div ref={mapElement} style={{ width: "100%", height: "400px" }}></div>
+    <div>
+      <div ref={mapElement} style={{ width: "100%", height: "500px" }} />
+
+      <button onClick={getCenter}>Get Center</button>
+    </div>
   );
 };
 
