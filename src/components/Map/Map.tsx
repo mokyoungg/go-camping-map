d9b671d80c5e8fa7362977fa04fe05f0478892ac
mokyoungg <mokyoungg@gmail.com>
2024-08-21 18:06:25 +0900

    feat: get location based data with search keyword

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,8 +1,14 @@
-import { useEffect, useRef, useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getLocationBasedList } from "../../api/camping";
 import Marker from "./Marker/Marker";
 import ReactDOMServer from "react-dom/server";
+import Input from "../UI/Input/Input";
+import Button from "../UI/Button/Button";
+import styles from "./Map.module.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
 
 const Map = () => {
   const { naver } = window;
@@ -17,10 +23,11 @@ const Map = () => {
     lat: 37.5665,
     lng: 126.978,
   });
+
   const [map, setMap] = useState<naver.maps.Map | null>(null);
 
   const { data: locationList } = useQuery({
-    queryKey: ["location-list", mapCenter],
+    queryKey: ["location-list", mapCenter.lat, mapCenter.lng],
     queryFn: () =>
       getLocationBasedList({
         pageNo: 1,
@@ -91,8 +98,49 @@ const Map = () => {
     setMapCenter({ lat, lng });
   };
 
+  const [keyword, setKeyword] = useState("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.target.value);
+  };
+
+  const searchLocation = async () => {
+    if (!naver || !map) return;
+
+    naver.maps.Service.geocode({ query: keyword }, (status, response) => {
+      if (status === naver.maps.Service.Status.ERROR) {
+        return alert("Something wrong!");
+      }
+
+      const resAddress = response.v2.addresses[0];
+
+      const newMapCenter = {
+        lat: Number(resAddress.x),
+        lng: Number(resAddress.y),
+      };
+
+      setMapCenter(newMapCenter);
+      map.setCenter(new naver.maps.LatLng(newMapCenter.lng, newMapCenter.lat));
+      centerRef.current.lat = newMapCenter.lat;
+      centerRef.current.lng = newMapCenter.lng;
+
+      getCenter();
+    });
+  };
+
   return (
-    <div>
+    <div className={cx("container")}>
+      <div className={cx("search-container")}>
+        <Input
+          placeholder="지역 검색"
+          value={keyword}
+          onChange={handleChange}
+        />
+        <Button size="small" onClick={searchLocation}>
+          검색
+        </Button>
+      </div>
+
       <div ref={mapElement} style={{ width: "100%", height: "500px" }} />
 
       <button onClick={getCenter}>Get Center</button>
