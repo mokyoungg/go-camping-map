01a04b9394c84573834b5d8aadc752895f37fc3e
mokyoungg <mokyoungg@gmail.com>
2024-08-26 17:08:10 +0900

    feat: update CampingListItem UI

--- src/components/CampingList/CampingListItem/CampingListItem.tsx
+++ src/components/CampingList/CampingListItem/CampingListItem.tsx
@@ -11,17 +11,42 @@ interface CampingListItemProps {
 const CampingListItem = (props: CampingListItemProps) => {
   const { data } = props;
 
-  const { facltNm, addr1, intro, homepage, tel } = data;
+  const {
+    facltNm,
+    addr1,
+    intro,
+    homepage,
+    tel,
+    posblFcltyCl,
+    sbrsCl,
+    themaEnvrnCl,
+  } = data;
 
   return (
     <div className={cx("container")}>
-      <div className={cx("name")}>{facltNm}</div>
-      <div className={cx("intro")}>{intro}</div>
+      <div className={cx("main-info")}>
+        <div className={cx("infos")}>
+          <div className={cx("name")}>{facltNm}</div>
+          <div className={cx("address")}>{addr1}</div>
+        </div>
+        <div className={cx("detail-infos")}>
+          <div className={cx("tel")}>{tel}</div>
+          <a className={cx("homepage")} href={homepage}>
+            {homepage}
+          </a>
+        </div>
+      </div>
 
-      <div className={cx("info")}>
-        <div className={cx("address")}>{addr1}</div>
-        <div className={cx("homepage")}>{homepage}</div>
-        <div className={cx("tel")}>{tel}</div>
+      {intro && <div className={cx("intro")}>{intro}</div>}
+
+      <div className={cx("sub-info")}>
+        {posblFcltyCl && (
+          <div className={cx("chip", "chip--green")}>{posblFcltyCl}</div>
+        )}
+        {sbrsCl && <div className={cx("chip", "chip--yellow")}>{sbrsCl}</div>}
+        {themaEnvrnCl && (
+          <div className={cx("chip", "chip--blue")}>{themaEnvrnCl}</div>
+        )}
       </div>
     </div>
   );
