1266d84896a6b1772d25473967a176978e79d93e
mokyoungg <mokyoungg@gmail.com>
2024-08-26 14:33:24 +0900

    feat: create camping list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
+import styles from "./App.module.scss";
+import classNames from "classnames/bind";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import BasedList from "./components/BasedList/BasedList";
+import CampingList from "./components/CampingList/CampingList";
 import Map from "./components/Map/Map";
+
+const cx = classNames.bind(styles);
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      {/* <BasedList /> */}
-      <Map />
+      <div className={cx("container")}>
+        <Map />
+        <CampingList />
+      </div>
     </QueryClientProvider>
   );
 }
---
+++ src/components/CampingList/CampingList.tsx
@@ -0,0 +1,24 @@
+import styles from "./CampingList.module.scss";
+import classNames from "classnames/bind";
+import CampingListItem from "./CampingListItem/CampingListItem";
+import useLocationList from "../../hooks/useLocationList";
+
+const cx = classNames.bind(styles);
+
+const CampingList = () => {
+  const { locationBasedList } = useLocationList();
+
+  console.log("lo :", locationBasedList);
+
+  return (
+    <ul className={cx("container")}>
+      {locationBasedList.map((item) => (
+        <li key={item.contentId}>
+          <CampingListItem data={item} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default CampingList;
