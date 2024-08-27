0192d7381b1482dfd042ce7a2e8be3e3175da7c6
mokyoungg <mokyoungg@gmail.com>
2024-08-27 16:49:24 +0900

    feat: handle list view with button

--- src/components/CampingList/CampingList.tsx
+++ src/components/CampingList/CampingList.tsx
@@ -2,20 +2,28 @@ import styles from "./CampingList.module.scss";
 import classNames from "classnames/bind";
 import CampingListItem from "./CampingListItem/CampingListItem";
 import useLocationList from "../../hooks/useLocationList";
+import { useLayoutStore } from "../../store/layout";
 
 const cx = classNames.bind(styles);
 
 const CampingList = () => {
   const { locationBasedList } = useLocationList();
+  const { isListOpen } = useLayoutStore();
+
+  console.log("is L :", isListOpen);
 
   return (
-    <ul className={cx("container")}>
-      {locationBasedList.map((item) => (
-        <li key={item.contentId}>
-          <CampingListItem data={item} />
-        </li>
-      ))}
-    </ul>
+    <>
+      {isListOpen ? (
+        <ul className={cx("container")}>
+          {locationBasedList.map((item) => (
+            <li key={item.contentId}>
+              <CampingListItem data={item} />
+            </li>
+          ))}
+        </ul>
+      ) : null}
+    </>
   );
 };
 
